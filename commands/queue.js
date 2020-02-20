c44f016cf5210f5d5956b35a7bdc9b194a0af470
TacticalTechJay <gogojayvin923@gmail.com>
2020-02-19 19:41:02 -0800

    I fixed markdown in song titles

--- commands/queue.js
+++ commands/queue.js
@@ -6,16 +6,17 @@ module.exports = {
     testing: false,
     aliases: ['q', 'playingsoon'],
     execute(message, a, client) {
+        const { Util } = require('discord.js');
         const serverQueue = client.queue.get(message.guild.id);
         let i = 1;
         if (!serverQueue) return message.channel.send('The queue is empty! I think it is about time to add songs, don\'t you think?');
         const queue = JSON.parse(JSON.stringify(serverQueue));
         queue.songs.shift();
-        return message.channel.send(`**Now Playing:** ${serverQueue.songs[0].info.title}\n__**Song queue:**__\n${queue.songs.length < 1 ? 'Nothing left. What now?' : queue.songs.map(song => `${i++} **-** ${song.info.title}`).join('\n')}`).catch(e => {
+        return message.channel.send(`**Now Playing:** ${serverQueue.songs[0].info.title}\n__**Song queue:**__\n${queue.songs.length < 1 ? 'Nothing left. What now?' : queue.songs.map(song => `${i++} **-** ${Util.escapeMarkdown(song.info.title)}`).join('\n')}`).catch(e => {
             if (e.message == 'Invalid Form Body\ncontent: Must be 2000 or fewer in length.') {
                 i = 1;
                 queue.songs.length = 10;
-                message.channel.send(`**Now Playing:** ${serverQueue.songs[0].info.title}\n__**Song queue:**__\n${queue.songs.map(song => `${i++} **-** ${song.info.title}`).join('\n')}\n**Length:** ${serverQueue.songs.length} songs`);
+                message.channel.send(`**Now Playing:** ${serverQueue.songs[0].info.title}\n__**Song queue:**__\n${queue.songs.map(song => `${i++} **-** ${Util.escapeMarkdown(song.info.title)}`).join('\n')}\n**Length:** ${serverQueue.songs.length} songs`);
             }
             else {console.error(e)};
         });

828503097d1905bb2ac6b5ebed69816aea11a316
TacticalTechJay <gogojayvin923@gmail.com>
2020-02-27 14:22:12 -0800

    New developer only command. Might be very useful.

---
+++ commands/restart.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'restart',
+    description: 'null',
+    args: false,
+    guildOnly: false,
+    async execute(message, args) {
+        if (message.author.id !== '127888387364487168') return;
+        await message.channel.send('I will now die. <a:loading:416475652922015746>');
+        process.exit(1);
+    }
+}

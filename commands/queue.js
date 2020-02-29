ba1a9ff875f5d91241e90d6bc232bdafb5e496b4
TacticalTechJay <gogojayvin923@gmail.com>
2020-02-28 18:45:55 -0800

    You can now see the FULLLL queue.

--- commands/queue.js
+++ commands/queue.js
@@ -14,9 +14,17 @@ module.exports = {
         queue.songs.shift();
         return message.channel.send(`**Now Playing:** ${serverQueue.songs[0].info.title}\n__**Song queue:**__\n${queue.songs.length < 1 ? 'Nothing left. What now?' : queue.songs.map(song => `${i++} **-** ${Util.escapeMarkdown(song.info.title)}`).join('\n')}`).catch(e => {
             if (e.message == 'Invalid Form Body\ncontent: Must be 2000 or fewer in length.') {
-                i = 1;
+                let i = 1;
                 queue.songs.length = 10;
-                message.channel.send(`**Now Playing:** ${serverQueue.songs[0].info.title}\n__**Song queue:**__\n${queue.songs.map(song => `${i++} **-** ${Util.escapeMarkdown(song.info.title)}`).join('\n')}\n**Length:** ${serverQueue.songs.length} songs`);
+                const body = serverQueue.songs.map(s => `${i++}: ${s.info.title}`).join('\n');
+                const res = await fetch('https://hasteb.in/documents', {
+                    method: 'POST',
+                    body: body,
+                    headers: { 'Content-Type': 'text/plain' }
+                });
+                i = 1;
+                const { key } = await res.json();
+                message.channel.send(`**Now Playing:** ${serverQueue.songs[0].info.title}\n__**Song queue:**__\n${queue.songs.map(song => `${i++} **-** ${Util.escapeMarkdown(song.info.title)}`).join('\n')}\n**Length:** ${serverQueue.songs.length} songs\nThe rest of the queue can be found below: https://hasteb.in/${key}.js`);
             }
             else {console.error(e);}
         });

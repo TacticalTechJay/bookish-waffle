d1c9e5d9695121d29090adcb3b718446e581fff4
TacticalTechJay <gogojayvin923@gmail.com>
2020-03-14 15:06:27 -0700

    Made a response for not being the same voice channel a bit more proper.

--- commands/play.js
+++ commands/play.js
@@ -11,7 +11,7 @@ module.exports = {
     async execute(message, args, client) {
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel!');
         if (message.guild.me.voice.channel) {
-            if (message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send('No.');
+            if (message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send('You need to be in the same voice channel as me to use this command.');
         }
 		if (!message.guild.me.hasPermission(['SPEAK', 'CONNECT', 'VIEW_CHANNEL'])) return message.channel.send('I do not have the required permissions to play music');
         if (!message.member.voice.channel.permissionsFor(message.guild.me).has(['SPEAK', 'CONNECT', 'VIEW_CHANNEL'])) return message.channel.send('I do not have the required permisssions to play music');

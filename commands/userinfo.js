9396e18ff455cbe5d80af9c6d6b25cc2a03bac7d
TacticalTechJay <gogojayvin923@gmail.com>
2020-03-24 19:15:03 -0700

    Improved userinfo command.

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -16,40 +16,26 @@ module.exports = {
                 .setTitle(`${user.user.username}#${user.user.discriminator} (${user.id})`)
                 .addField('Bot', user.user.bot ? 'Yes' : 'No', true)
                 .addField('Nickname', user.nickname ? user.nickname : 'No nickname', true)
-                .addField('Presence', user.presence.status, true)
+                .addField('Presence', user.presence.status.titleCase(), true)
                 .addField('Created Date', require('moment')(user.user.createdAt).format('MMMM Do[,] YYYY'), true)
                 .addField('Joined Date', require('moment')(user.joinedAt).format('MMMM Do[,] YYYY'), true)
-		.addField(`Roles (${--message.member.roles.cache.size})`, message.member.roles.cache ? message.member.roles.cache.map(x => x.toString()).join(' ').substring(0, 1024).replace(/\s\S+[^>]$/, '') : 'None')
+		        .addField(`Roles (${--message.member.roles.cache.size})`, message.member.roles.cache ? message.member.roles.cache.map(x => x.toString()).join(' ').substring(0, 1024).replace(/\s\S+[^>]$/, '') : 'None')
                 .setColor(0x679f28)
                 .setThumbnail(user.user.displayAvatarURL());
             return message.channel.send(embed);
         }
-        let user = await message.mentions.members.first() || await message.guild.members.cache.get(args[0]);
-        if (!user) {
-            user = await client.users.fetch(args[0]);
-            const embed = new MessageEmbed()
-                .setColor(0x0cecf7)
-                .setTitle(`${user.username}#${user.discriminator} (${user.id})`)
-                .addField('Bot', user.bot ? 'Yes' : 'No', true)
-                .addField('Created Date', require('moment')(user.createdAt).format('MMMM Do[,] YYYY'), true)
-                .setColor(0x5ea6db)
-                .setThumbnail(user.displayAvatarURL());
-            message.channel.send('Not much can be found except this. This user was not in guild:');
-            return message.channel.send(embed);
-         }
-        else {
-            const embed = new MessageEmbed()
-                .setColor(0x12db37)
-                .setTitle(`${user.user.username}#${user.user.discriminator} (${user.id})`)
-                .addField('Bot', user.user.bot ? 'Yes' : 'No', true)
-                .addField('Nickname', user.nickname ? user.nickname : 'No nickname', true)
-                .addField('Presence', user.presence.status, true)
-                .addField('Created Date', require('moment')(user.user.createdAt).format('MMMM Do[,] YYYY'), true)
-                .addField('Joined Date', `${user.joinedAt ? require('moment')(user.joinedAt).format('MMMM Do[,] YYYY') : 'N/A'}`, true)
-		.addField(`Roles (${--user.roles.cache.size})`, user.roles.cache ? user.roles.cache.map(x => x.toString()).join(' ').substring(0, 1024).replace(/\s\S+[^>]$/, '') : 'None')
-                .setColor(0x679f28)
-                .setThumbnail(user.user.displayAvatarURL());
-            return message.channel.send(embed);
-        }
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(m => m.user.username.includes(args.join(" "))) || await client.users.fetch(args[0]);
+        const embed = new MessageEmbed()
+            .setColor(0x12db37)
+            .setTitle(`${user.user ? user.user.tag : user.tag} (${user.id})`)
+            .addField('Bot', user.user ? user.user.bot  ? 'Yes' : 'No' : user.bot ? 'Yes' : 'No', true)
+            .addField('Nickname', user.nickname ? user.nickname : 'N/A', true)
+            .addField('Presence', user.presence.status.titleCase(), true)
+            .addField('Created Date', require('moment')(user.user ? user.user.createdAt : user.createdAt).format('MMMM Do[,] YYYY'), true)
+            .addField('Joined Date', `${user.joinedAt ? require('moment')(user.joinedAt).format('MMMM Do[,] YYYY') : 'N/A'}`, true)
+            .setColor(0x679f28)
+            .setThumbnail(user.user ? user.user.displayAvatarURL() : user.displayAvatarURL());
+		if (user.roles) embed.addField(`Roles (${--user.roles.cache.size})`, user.roles.cache ? user.roles.cache.map(x => x.toString()).join(' ').substring(0, 1024).replace(/\s\S+[^>]$/, '') : 'None')
+        return message.channel.send(embed);
     }
 };

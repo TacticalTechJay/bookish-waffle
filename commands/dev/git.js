e81646f63cc72b1c789bf31ca78d859be099349a
TacticalTechJay <gogojayvin923@gmail.com>
2020-04-21 21:59:36 +0200

    Best way to stay up to date with commands and the like is to do it yourself.

---
+++ commands/dev/git.js
@@ -0,0 +1,22 @@
+const { execSync } = require('child_process');
+const { sys } = 
+
+module.exports = {
+    name: 'git',
+    description: null,
+    category: 'useful',
+    cooldown: 1,
+    group: 'trusted',
+    async execute(message) {
+        if (!sys.collaborators.includes(message.author.id)) return;
+        try {
+            const executedPiece = await execSync('git pull').toString();
+            if (executedPiece.startsWith('Already up to date')) return message.channel.send('The focuesed branch is up to date.');
+            if (client.manager.players.size == 0) return message.channel.send('There are currently people vibing to music. Try again later.');
+            await message.channel.send(`\`\`\`${executedPiece}\`\`\``);
+            process.exit(1);
+        } catch (err) {
+            message.author.send(err);
+        }
+    }
+}

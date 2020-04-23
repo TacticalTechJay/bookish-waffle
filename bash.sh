2fffac95dbfaabef0545d51ce876978600e5830c
Damian Freeman <59120701+SuperCoderDamian@users.noreply.github.com>
2020-04-22 21:42:26 -0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 # bash build.sh && echo "nou"
 echo ".sh script has been ran yeet."
 node index.js
-# pm2 start index.js --name "Earth-chan"
+# optional if using process manager: pm2 start index.js --name "Earth-chan"

a2f44cee1603d2ef6e1f54b7c15b54622caa3dd0
TacticalTechJay <gogojayvin923@gmail.com>
2020-05-13 19:45:48 +0200

    Just a quick test. Will be removed later.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-String.prototype.toProperCase = function () {
+String.prototype.toProperCase = function() {
     return this.toLowerCase().replace(/(^|[\s.])[^\s.]/gm, (s) => s.toUpperCase());
-}
+};
 
-new (require('./structures/Extends'))()
+new (require('./structures/Extends'))();
 
-const EarthClient = require("./structures/EarthClient");
+const EarthClient = require('./structures/EarthClient');
 const config = require('../config.json');
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: config.sentry.dsn });
 
 const client = new EarthClient(process.env.DEVELOPMENT ? config.tokens.dev : config.tokens.prod, {
     ws: {
-        intents: 1925
+        intents: 1927
     },
     messageCacheMaxSize: 100,
     messageCacheLifetime: 3600,
     messageSweepInterval: 7200,
+    fetchAllMembers: true,
     disableEveryone: true
 });
 

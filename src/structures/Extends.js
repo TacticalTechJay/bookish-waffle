dd50a1a0856ba71ca61274f5fdc29b66b3e53e46
TacticalTechJay <gogojayvin923@gmail.com>
2020-06-01 09:17:44 +0200

    ESLint won't get off my back for this one

--- src/structures/Extends.js
+++ src/structures/Extends.js
@@ -2,7 +2,7 @@ const { Structures } = require('discord.js');
 const fs = require('fs');
 
 class Extends {
-    constructor(client) {
+    constructor() {
         for (const struct of fs.readdirSync('src/structures/extensions')) Structures.extend(struct.slice(0, -3), require('./extensions/' + struct));
     }
 }

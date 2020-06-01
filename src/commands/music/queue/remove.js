febc29dc3d80e30695cb7ed3e6637bd6be30514c
TacticalTechJay <gogojayvin923@gmail.com>
2020-06-01 09:25:22 +0200

    Stop breaking things on me.

--- src/commands/music/queue/remove.js
+++ src/commands/music/queue/remove.js
@@ -17,7 +17,8 @@ module.exports = class Remove extends Command {
             if (!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to use this command!');
             if (!message.guild.me.voice.channel) return message.channel.send('I am not in a voice channel. :thinking:');
             if (message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send('You need to be in the same voice channel as me to use this command!');
-            if (message.guild.player.songs.length <= 1) return message.channel.send('That ain\'t happening.');
+            if (message.guild.player.songs.length <= 1) return message.channel.send('That ain\'t happening. Add more music then I will consider it.');
+            if (message.guild.player.position === message.guild.player.songs.length) return message.channel.send('That ain\'t happening. You are already on that song. Try using skip perhaps.');
             message.guild.player.songs.pop();
             return message.channel.send('Removed the last song of the queue.');
         }
@@ -25,9 +26,9 @@ module.exports = class Remove extends Command {
         --toRemove;
         if (!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to use this command!');
         if (!message.guild.me.voice.channel) return message.channel.send('I am not in a voice channel. :thinking:');
-        if (!toRemove) return message.channel.send('That is not a number. :neutral_face:');
+        if (isNaN(toRemove)) return message.channel.send('That is not a number. :neutral_face:');
         if (message.member.voice.selfDeaf) return message.channel.send('You need to be undeafened to use something like this.');
-        if (toRemove <= 1 || toRemove <= message.guild.player.np) return message.channel.send('You can\'t remove that! It could break things!');
+        if (toRemove <= 0 || toRemove <= message.guild.player.position) return message.channel.send('You can\'t remove that! It could break things!');
         if (toRemove > message.guild.player.songs.length) return message.channel.send('There is no song in that spot.');
         if (message.guild.player.loaded && message.guild.player.loaded.locked && message.guild.player.loaded.user !== message.author.id) return message.channel.send('The queue is locked and loaded and it is aimed at you for interrupting this vibe.');
         message.guild.player.songs.splice(toRemove, 1);

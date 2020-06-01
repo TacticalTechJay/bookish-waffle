fe0ff570a8993fb5869cbc7d75d81af5a78f237a
TacticalTechJay <gogojayvin923@gmail.com>
2020-06-01 05:54:50 +0200

    New command! Notifications!

---
+++ src/commands/music/queue/notifications.js
@@ -0,0 +1,19 @@
+const Command = require('../../../structures/Command');
+
+module.exports = class notifications extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'notifications',
+            description: 'Toggle the now playing notifications.',
+            aliases: ['notifs', 'n']
+        });
+    }
+
+    async exec(message) {
+        if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to commit vibe loop.');
+        if (!message.guild.player) return message.channel.send('There is no player.');
+        if (message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send('You can\'t run that in a channel with no vibe.');
+        message.guild.player.settings.notifications ? message.guild.player.settings.notifications = false : message.guild.player.settings.notifications = true;
+        return message.channel.send(`Notifications are now ${message.guild.player.settings.notifications ? 'enabled' : 'disabled'}.`);
+    }
+};

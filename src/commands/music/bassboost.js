39e70dd9f7106d336f05e7da0a55c4f4dbfd356f
TacticalTechJay <gogojayvin923@gmail.com>
2020-06-03 04:02:11 +0200

    Bassboost is fixed

--- src/commands/music/bassboost.js
+++ src/commands/music/bassboost.js
@@ -7,7 +7,7 @@ module.exports = class Volume extends Command {
             category: 'music',
             aliases: ['bb'],
             description: 'Get some bass, so you can vibe harder.',
-            usage: '[Level = highaf, high, medium, low, none]'
+            usage: '<max, high, medium, low, none, treble>'
         });
     }
 
@@ -17,11 +17,12 @@ module.exports = class Volume extends Command {
         if (message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send('You can\'t run that in a channel with no vibe.');
         if (message.guild.player && message.guild.player.loaded && message.guild.player.loaded.locked && message.guild.player.loaded.user !== message.author.id) return message.channel.send('This queue is locked, therefore you can\'t modify it.');
 
-        const presets = { highaf: 1.5, high: 0.25, medium: 0.15, low: 0.05, none: -0.10 };
+        const presets = { max: 1, high: 0.25, medium: 0.15, low: 0.05, none: 0, treble: -0.25 };
         if (!args[0]) return message.channel.send(`Please specify a preset: ${Object.keys(presets).join(', ')}`);
-        if (!presets[args[0].toLowerCase()]) return message.channel.send('Looks like that prefix doesn\'t exist!');
-        const bands = [{ band: 0, presets: presets[args[0].toLowerCase()] }, { band: 1, presets: presets[args[0].toLowerCase()] }, { band: 2, presets: presets[args[0].toLowerCase()] }];
-        await message.guild.player.node.send({ op: 'equalizer', guildId: message.guild.id, bands });
+        if (!presets[args[0].toLowerCase()] && isNaN(Number(args[0]))) return message.channel.send('Looks like that preset doesn\'t exist!');
+        if (Number(args[0]) < -0.25 || Number(args[0]) > 1) return message.channel.send('There would be major problems with this. I won\'t allow that.');
+        const bands = [{ band: 0, gain: presets[args[0].toLowerCase()] || Number(args[0]) }, { band: 1, gain: presets[args[0].toLowerCase()] || Number(args[0]) }, { band: 2, gain: presets[args[0].toLowerCase()] || Number(args[0]) }];
+        await message.guild.player.equalizer(bands);
         return message.channel.send(`Alright, the bassboost level is now **${args[0].toLowerCase()}**`);
     }
 };

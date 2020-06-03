da21ba4c872a88ccfcc8bfc5441dfa985e982645
TacticalTechJay <gogojayvin923@gmail.com>
2020-06-03 04:32:47 +0200

    Improved this command. It's far superior than the older one.

--- src/commands/system/serverinfo.js
+++ src/commands/system/serverinfo.js
@@ -17,23 +17,26 @@ module.exports = class ServerInfo extends Command {
             .setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
             .setThumbnail(message.guild.iconURL({ dynamic: true, size: 2048 }))
             .addField('Server Information', `
-**Name:** ${Util.escapeMarkdown(message.guild.name)}
-**ID:** ${message.guild.id}
-**Owner:** ${message.guild.owner.toString()}
-**Verification:** ${message.guild.verificationLevel.toProperCase()}
-**Partner Status:** ${message.guild.partnered ? 'Partnered' : 'Not partnered'}
-**Nitro Boost:** Level **${message.guild.premiumTier}**, with **${message.guild.premiumSubscriptionCount}** Boosts
-**Created At:** ${moment.utc(message.guild.createdAt).format('LLL')} (${moment.utc(message.guild.createdAt).fromNow()})
-`).addField(`Users (${message.guild.memberCount})`, `
-**Online:** ${message.guild.members.cache.filter(m => m.user.presence.status === 'online').size}
-**Offline:** ${message.guild.members.cache.filter(m => m.user.presence.status === 'offline').size}
-**Do not Disturb:** ${message.guild.members.cache.filter(m => m.user.presence.status === 'dnd').size}
-**Idling:** ${message.guild.members.cache.filter(m => m.user.presence.status === 'idle').size}
-`, true).addField(`Channels (${message.guild.channels.cache.size})`, `
-**Text:** ${message.guild.channels.cache.filter(m => m.type === 'text').size}
-**Voice:** ${message.guild.channels.cache.filter(m => m.type === 'voice').size}
-**Categories:** ${message.guild.channels.cache.filter(m => m.type === 'category').size}
-`, true).addField(`Roles (${message.guild.roles.cache.size})`, message.guild.roles.cache.map(x => x.toString()).join(' ').substring(0, 1024).replace(/\s\S+[^>]$/, ''));
+                **Name:** ${Util.escapeMarkdown(message.guild.name)}
+                **ID:** ${message.guild.id}
+                **Owner:** ${message.guild.owner.toString()}
+                **Verification:** ${message.guild.verificationLevel.toProperCase()}
+                **Partner Status:** ${message.guild.partnered ? 'Partnered' : 'Not partnered'}
+                **Nitro Boost:** Level **${message.guild.premiumTier}**, with **${message.guild.premiumSubscriptionCount}** Boosts
+                **Created At:** ${moment.utc(message.guild.createdAt).format('LLL')} (${moment.utc(message.guild.createdAt).fromNow()})
+            `)
+            .addField(`Users (${message.guild.memberCount})`, `
+                **Online:** ${message.guild.members.cache.filter(m => m.user.presence.status === 'online').size}
+                **Offline:** ${message.guild.members.cache.filter(m => m.user.presence.status === 'offline').size}
+                **Do not Disturb:** ${message.guild.members.cache.filter(m => m.user.presence.status === 'dnd').size}
+                **Idling:** ${message.guild.members.cache.filter(m => m.user.presence.status === 'idle').size}
+            `, true)
+            .addField(`Channels (${message.guild.channels.cache.size})`, `
+                **Text:** ${message.guild.channels.cache.filter(m => m.type === 'text').size}
+                **Voice:** ${message.guild.channels.cache.filter(m => m.type === 'voice').size}
+                **Categories:** ${message.guild.channels.cache.filter(m => m.type === 'category').size}
+            `, true)
+            .addField(`Roles (${message.guild.roles.cache.size})`, message.guild.roles.cache.map(x => `<@&${x.id}>`).join(' ').substring(0, 1024).replace(/\s\S+[^>]$/, ''));
         message.channel.send(embed);
     }
 };
